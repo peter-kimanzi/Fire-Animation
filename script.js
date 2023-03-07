dc9c2b15760700c7afe56c9080518d0774e17406
peter kimanzi <kimjayden001@gmail.com>
2023-03-07 10:51:27 +0300

    javascript correction water

--- script.js
+++ script.js
@@ -484,7 +484,12 @@ particlesJS("particles-js", {
         "color": "#000000"
       },
      
-
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
 
 
   
