e2e97eef46e359024984fc5139502fdd46aaa7cb
peter kimanzi <kimjayden001@gmail.com>
2023-03-07 10:32:41 +0300

    javascript correction

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const canvas = window.canvas
 const gl = canvas.getContext("webgl2")
 const dpr = Math.max(1, .5*window.devicePixelRatio)
 /** @type {Map<string,PointerEvent>} */
+const touches = new Map()
+
+const vertexSource = `#version 300 es
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+
+
 
 
 
