2605db8616b461870570c61936b8e8a4e78f5217
peter kimanzi <kimjayden001@gmail.com>
2023-03-07 10:32:49 +0300

    javascript correction

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const vertexSource = `#version 300 es
 #ifdef GL_FRAGMENT_PRECISION_HIGH
 precision highp float;
 #else
+precision mediump float;
+#endif
+
+in vec2 position;
+
+
+out vec4 waterColor;
+
+
 
 
 
